91419a282ccc46ffea6452bef1c07180cb0fbb57
binamkayastha <binamkayastha@gmail.com>
2016-10-09 13:42:37 -0400

    Added psuedocode

--- BDB.c
+++ BDB.c
@@ -3,5 +3,15 @@
 int main() {
     printf("Hello World!\n");
 
+    /*
+     * Take arg as shell script file input.
+     * Prompt user for next action
+     *  -> step: run one line in shell script
+     *  -> break #: add breakpoint at line #
+     *  -> continue: run till completion or reach breakpoint
+     *  -> printf "%s/n" "var": prints the variable (run bash command)
+     *  -> anything else: run bash command
+     */
+
     return (0); //Sucess
 }
