04f1d6d213c1e1409d73515e976bf019b86d6df7
binamkayastha <binamkayastha@gmail.com>
2016-11-17 23:48:36 -0500

    Updated BDB code to read each line in given script, and print them out with line numbers

--- BDB.c
+++ BDB.c
@@ -1,8 +1,7 @@
+#include <stdlib.h>
 #include <stdio.h>
 
-int main() {
-    printf("Hello World!\n");
-
+int main(int argc, char *argv[]) {
     /*
      * Take arg as shell script file input.
      * Prompt user for next action
@@ -12,6 +11,43 @@ int main() {
      *  -> printf "%s/n" "var": prints the variable (run bash command)
      *  -> anything else: run bash command
      */
+    // If there isn't one arguement
+    if(argc != 2) {
+        printf("Usage: ./BDB \"filename\"\n");
+        return(1); // Failure
+    } else {
+        printf("Analyzing script %s...\n", argv[1]);
+    }
+
+    // Try to open file
+    FILE *file;
+    char *filename = argv[1];
+
+    file = fopen(filename, "r");
+
+    if(file == NULL) { // Throw error if not found
+        perror(filename);
+    }
+
+
+    // lineBuffer, assuming that the line in the script isn't goin to be more than 1024 characters long.
+    char *lineBuffer = (char *) malloc(sizeof(char)* 1024);
+    // Size of lineBuffer
+    int lineBufferMaxSize = sizeof(char) * 1024;
+
+
+
+    // Used to keep track of line number in code
+    int lineNum = 1;
+
+    if(file) {
+        while (fgets(lineBuffer, lineBufferMaxSize, file) != NULL) {
+            printf("%i: %s", lineNum, lineBuffer);
+            lineNum++;
+        }
+        fclose(file);
+    }
+
 
     return (0); //Sucess
 }
